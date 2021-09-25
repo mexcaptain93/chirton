50282bd3f55b1a3627110c63b63fb9029b198c5c
mexcaptain93 <mexcaptain@gmail.com>
2021-09-25 19:51:57 +0300

    Header tablet

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,14 @@
-$(document).ready(function () {
-    svg4everybody({});
-});
+$(function(){
+    searchPlaceholder()
+})
+
+$(window).on('resize', function(){
+    searchPlaceholder()
+})
+
+function searchPlaceholder() {
+    input = $('.js-header-serach-input')
+    placeholder = ($(window).width() <= 767) ? 'Поиск' : 'Название или артикул товара'
+
+    input.attr('placeholder', placeholder)
+}
