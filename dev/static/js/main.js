d8c6924f01017b785566f9810a3c8408ba80e16d
mexcaptain93 <mexcaptain@gmail.com>
2021-11-08 23:57:53 +0300

    Header search hint popup + old changes

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,6 +1,8 @@
 $(function(){
     searchPlaceholder()
     productSlider()
+    select()
+    filter()
 })
 
 $(window).on('resize', function(){
@@ -18,5 +20,69 @@ function productSlider() {
     $('.js-product-slider').slick({
         prevArrow: '<a href="#" class="gallery__prev"></a>',
         nextArrow: '<a href="#" class="gallery__next"></a>'
+    })
+}
+
+function select() {
+    $('.select').each(function() {
+        const _this = $(this),
+            selectOption = _this.find('option'),
+            selectOptionLength = selectOption.length,
+            selectedOption = selectOption.filter(':selected'),
+            duration = 450; // длительность анимации
+
+        _this.hide();
+        _this.wrap('<div class="select"></div>');
+        $('<div>', {
+            class: 'new-select',
+            text: _this.children('option:eq(0)').text()
+        }).insertAfter(_this);
+
+        const selectHead = _this.next('.new-select');
+        $('<div>', {
+            class: 'new-select__list'
+        }).insertAfter(selectHead);
+
+        const selectList = selectHead.next('.new-select__list');
+        for (let i = 0; i < selectOptionLength; i++) {
+            $('<div>', {
+                class: 'new-select__item',
+                html: $('<span>', {
+                    text: selectOption.eq(i).text()
+                })
+            })
+                .attr('data-value', selectOption.eq(i).val())
+                .appendTo(selectList);
+        }
+
+        const selectItem = selectList.find('.new-select__item');
+        selectList.slideUp(0);
+        selectHead.on('click', function() {
+            if ( !$(this).hasClass('on') ) {
+                $(this).addClass('on');
+                selectList.slideDown(duration);
+
+                selectItem.on('click', function() {
+                    let chooseItem = $(this).data('value');
+
+                    $('select').val(chooseItem).attr('selected', 'selected');
+                    selectHead.text( $(this).find('span').text() );
+
+                    selectList.slideUp(duration);
+                    selectHead.removeClass('on');
+                });
+
+            } else {
+                $(this).removeClass('on');
+                selectList.slideUp(duration);
+            }
+        });
+    });
+}
+
+function filter() {
+    $('.filter__title').on('click', function () {
+        $(this).siblings('.filter__values').slideToggle()
+        $(this).parent().toggleClass('filter__param_opened', 500)
     })
 }
